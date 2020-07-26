a5a2cdd230a05b820a74ca7a5aed9c4ac2556465
Jayanth Apagundi <42775370+JayanthApagundi@users.noreply.github.com>
2020-07-27 01:11:15 +0530

    4 patterns done

--- Patterns/main.c
+++ Patterns/main.c
@@ -3,45 +3,49 @@
 
 int main()
 {
-    int n;
-    printf("Enter value of n \n");
-    scanf("%d",&n);
-    //Generate's square
-    for(int i=0;i<n;i++)
-    {
-       for(int j=0;j<n;j++)
-        printf("* ");
-       printf("\n");
-    }
-    printf("--------------------\n");
-    //Generate's inverted right angled triangle
-    for(int i=0;i<n;i++)
-    {
-       for(int j=i;j<n;j++)
-        printf("* ");
-       printf("\n");
-    }
-     printf("-------------------\n");
-     //Generate's right angled triangle
-     for(int i=0;i<n;i++)
-    {
-       for(int j=0;j<=i;j++)
-        printf("* ");
-       printf("\n");
-    }
-    //Generates triangle attached to right side
-    printf("--------------------\n");
-    for(int i=0;i<n;i++)
-    {
-       for(int j=0;j<i;j++)
-        printf("* ");
-       printf("\n");
-    }
-    for(int i=0;i<n;i++)
-    {
-       for(int j=i;j<n;j++)
-        printf("* ");
-       printf("\n");
-    }
-    printf("--------------------\n");
+   int n;
+   printf("Enter value of n \n");
+   scanf("%d", &n);
+
+   //Generate's square
+   for (int i = 0; i < n; i++)
+   {
+      for (int j = 0; j < n; j++)
+         printf("* ");
+      printf("\n");
+   }
+   printf("--------------------\n");
+
+   //Generate's inverted right angled triangle
+   for (int i = 0; i < n; i++)
+   {
+      for (int j = i; j < n; j++)
+         printf("* ");
+      printf("\n");
+   }
+   printf("-------------------\n");
+
+   //Generate's right angled triangle
+   for (int i = 0; i < n; i++)
+   {
+      for (int j = 0; j <= i; j++)
+         printf("* ");
+      printf("\n");
+   }
+
+   //Generates triangle attached to right side
+   printf("--------------------\n");
+   for (int i = 0; i < n; i++)
+   {
+      for (int j = 0; j < i; j++)
+         printf("* ");
+      printf("\n");
+   }
+   for (int i = 0; i < n; i++)
+   {
+      for (int j = i; j < n; j++)
+         printf("* ");
+      printf("\n");
+   }
+   printf("----------------------\n");
 }
